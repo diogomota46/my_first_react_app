6c71a89c330d216c57e9f5623635bf9d02ffbc4b
Rita Baptista Cabral <ritacabral@MBP-de-Rita.lan>
2023-01-21 01:21:18 +0000

    final version

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const movie1 = {
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -38,14 +39,14 @@ const App = () => {
             <div className='search'>
                 <input 
                     placeholder='Search for movies'
-                    value='Superman'
-                    onChange={() => {}}>
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}>
                 </input>
 
                 <img
                     src={SearchIcon}
                     alt='search'
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
